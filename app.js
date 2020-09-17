00258ad37f8d8b66844943248153bf9a1b8a902a
RehanRabbani <rabbanirehan6@gmail.com>
2020-09-17 20:12:57 +0500

    add connection with mongodb atlas

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 const app = express()
- 
+
+const dbUri = 'mongodb+srv://rehan:humtum123@node-rest-shop.299ce.mongodb.net/node?retryWrites=true&w=majority';
+mongoose.connect(dbUri, { useNewUrlParser: true,useUnifiedTopology: true })
+ .then( (result) => app.listen(3000))
+ .catch( (error) => console.log(error));
+
+app.get('/about-blog', (req , res) => {
+    const blog = new Blog({
+        title:'new Blog 2',
+        snippet:'about my new blog',
+        body:'more about blog'
+    });
+    
+    blog.save().then( (result) => {
+      res.send(result);
+    }).catch((error) => console.log(error))
+}) 
+
+app.get('/all-blog', (req, res) => {
+    Blog.find()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+})
+
+app.get('/single-blog' ,(req, res) => {
+    Blog.findById('5f637372c060289838de3bbf')
+    .then((result) =>{
+        res.send(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+})
+
 app.get('/', function (req, res) {
   res.sendFile('./views/index.html', { root : __dirname});
 });
@@ -18,4 +57,3 @@ app.use((req,res)=>{
 
 
 
-app.listen(3000)
